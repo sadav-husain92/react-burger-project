177f0bb0b8b8b6dd356de8f1d1273b4d84b7c3fb
sadav-husain92 <sadav.husain92@gmail.com>
2020-06-01 19:52:07 +0530

    This is my first react project

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationItem from './NavigationItem/NavigationItem';
+import classes from './NavigationItems.module.css';
+
+const navigationItems = (props) => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" >Burger Builder</NavigationItem>
+        <NavigationItem link="/" active>Checkout</NavigationItem>
+    </ul>
+);
+
+export default navigationItems;
